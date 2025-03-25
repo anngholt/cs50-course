902a75cca05ddb8209085d78df1f39ec6e78fffb
annagholt <anna.holt@protonmail.com>
2025-03-25 12:54:05 +0100

    add functions to count letters and words

--- readability/readability.c
+++ readability/readability.c
@@ -5,16 +5,23 @@
 
 char* get_text(void);
 int get_number_of_sentences(char* text);
+int get_number_of_letters(char* text);
+int get_number_of_words(char* text);
 
 int main(void)
 {
 
     // Prompt the user for some text
+    // Count the number of letters, words, and sentences in the text
     char* text = get_text();
     int sentences = get_number_of_sentences(text);
+    int letters = get_number_of_letters(text);
+    int words = get_number_of_words(text);
     printf("Number of sentences:  %i\n", sentences);
+    printf("Number of letters:  %i\n", letters);
+    printf("Number of words:  %i\n", words);
 
-    // Count the number of letters, words, and sentences in the text
+    
 
     // Compute the Coleman-Liau index
 
@@ -37,6 +44,34 @@ int get_number_of_sentences(char* text)
     for (int i = 0; i < size; i++)
     {
         if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+return count;
+}
+
+int get_number_of_letters(char* text)
+{
+    int count = 0;
+    int size = strlen(text);
+    for (int i = 0; i < size; i++)
+    {
+        if (isalpha(text[i]))  // Check if the character is a letter
+        {
+            count++;
+        }
+    }
+return count;
+}
+
+int get_number_of_words(char* text)
+{
+    int count = 1;
+    int size = strlen(text);
+    for (int i = 0; i < size; i++)
+    {
+        if (text[i] == ' ')  // Check if the character is a space
         {
             count++;
         }
