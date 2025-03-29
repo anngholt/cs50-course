a716b3ec90c42a47c759e482775b1204af8ebd40
annagholt <anna.holt@protonmail.com>
2025-03-29 13:34:34 +0100

    blur filter

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -37,6 +38,49 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sumRed = 0;
+            int sumBlue = 0;
+            int sumGreen = 0;
+            int count = 0;
+            int ni;
+            int nj;
+
+            //loops over all the neighboring pixels of pixel (i, j)
+            for (int di = -1; di < 2; di++)
+            {
+                for (int dj = -1; dj < 2; dj++)
+                {
+                    ni = i + di;
+                    nj = j + dj;
+
+                    if( ni > 0 && ni < height && nj > 0 && nj < width)
+                    {
+                        sumRed += image[ni][nj].rgbtRed;
+                        sumGreen += image[ni][nj].rgbtGreen;
+                        sumBlue += image[ni][nj].rgbtBlue;
+                        count ++;
+
+                    }
+                }
+            }
+            temp[i][j].rgbtRed = sumRed/count;
+            temp[i][j].rgbtGreen = sumGreen/count;
+            temp[i][j].rgbtBlue = sumBlue/count;
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {   
+            image[i][j] = temp[i][j];
+        }
+    }
     return;
 }
 
