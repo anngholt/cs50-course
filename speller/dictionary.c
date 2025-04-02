a3ee82482e9b1eef274680df29304e149a72ab5d
annagholt <anna.holt@protonmail.com>
2025-04-02 16:23:13 +0200

    final version. larger bucket size, upgraded hash function with prime number

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -15,23 +15,44 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 101;
 
 // Hash table
 node *table[N];
+//global variable for counting words
+unsigned int word_count = 0; 
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
-    return false;
+    //get index for the word
+    unsigned int index = hash(word);
+    node *ptr = table[index];
+    while (ptr != NULL)
+    {
+        if (strcasecmp(ptr->word, word) == 0)
+        {
+            return true;
+        }
+        ptr = ptr->next;
+
+    }
+    
+    return false; //not found
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    unsigned int hash_value = 0;
+    int i = 0;
+    while(word[i] != '\0')
+    { 
+        // use prime number to reduce collisions
+        hash_value = (hash_value * 31 + tolower(word[i])) % N;
+        i++;
+    }
+    return hash_value;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -54,17 +75,14 @@ bool load(const char *dictionary)
             fclose(source);
             return false;
         }
-
         // Add each word to the hash table
         strcpy(new_node->word, word);
         new_node->next = NULL;
         unsigned int index = hash(word);
         new_node->next = table[index];
         table[index] = new_node;
+        word_count++; 
     }
-
-    
-
     // Close the dictionary file
     fclose(source);
     return true; 
@@ -73,13 +91,25 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
+
 bool unload(void)
 {
-    // TODO
-    return false;
+    // Loop through each bucket in the hash table
+    for (int i = 0; i < N; i++)
+    {
+        node *ptr = table[i];  // Start at the beginning of the list
+
+        // Traverse the linked list, freeing nodes
+        while (ptr != NULL)
+        {
+            node *temp = ptr;  // Store current node
+            ptr = ptr->next;  // Move to next node
+            free(temp);  // Free previous node
+        }
+    }
+    return true;  // Memory successfully freed
 }
